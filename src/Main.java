22055ab9f2ada52f6529e67151c9203a3eb30b6b
Bhathiya Sahan <bhathiyasahanedu@gmail.com>
2023-07-24 15:48:24 +0530

    Update seat function with using multidimensional array

--- src/Main.java
+++ src/Main.java
@@ -11,48 +11,64 @@ import java.util.Scanner;  // Import Scanner class to code
 public class Main {
     public static void main(String[] args) {
 
-        String category;
+        float standard,pensione,frequent;
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("\n\t\t***************************************");
-        System.out.println("\t\t*   Welcome to Melbourne Cinema (MC)  *");       // Display welcome massage, "Melbourne Cinema (MC)" on the program
-        System.out.println("\t\t***************************************");
+        System.out.println("\n-------------------------------------------------");
+        System.out.println("\n\t***************************************");
+        System.out.println("\t*   Welcome to Melbourne Cinema (MC)  *");       // Display welcome massage, "Melbourne Cinema (MC)" on the program
+        System.out.println("\t***************************************");
         System.out.println("\n-------------------------------------------------");
 
         System.out.print("Please Enter Number of Rows : ");
         int number_of_rows = scan.nextInt();
         System.out.print("Please Enter The Date Of The Show : ");
         int date = scan.nextInt();
+
         scan.nextLine();
-        System.out.println("\nPrice Categories For A Seat");
-        System.out.println("\tStandard (S)");
-        System.out.println("\tPensioner (P)");
-        System.out.println("\tFrequent Patrons (F)");
-        System.out.print("Enter The Seat Price Category (S/P/F): ");
-        category = scan.next();
+        System.out.println("\nEnter The Seat Price Category");
+        System.out.print("\tStandard (S) $ : ");
+        standard = scan.nextFloat();
+        System.out.print("\tPensioner (P) $ : ");
+        pensione = scan.nextFloat();
+        System.out.print("\tFrequent Patrons (F) $ : ");
+        frequent = scan.nextFloat();
         System.out.println("\n-------------------------------------------------");
 
-        print_seat(number_of_rows);
+        String [][] seat_list = new String [number_of_rows][10];
 
+        for(int i=0; i<number_of_rows; i++){
+            for (int j=0; j<10; j++){
+                seat_list [i][j] = "O";
+            }
+        }
+        print_seat(seat_list,number_of_rows);
+
+//____________________________ Print Seat Function ----------------------------------------------------
     }
-    private static void print_seat(int Rows){
-        System.out.println("\n\t\t\t\t   *********");
-        System.out.println("\t\t\t\t   * STAGE *");             //  print out a header with the word "STAGE" surrounded by stars.
-        System.out.println("\t\t\t\t   *********\n");
+    private static void print_seat(String[][] seat_area, int Rows){
+        System.out.print("\n\t\t\t\t *********");
+        System.out.println("\t  \"X\"- Unavailable");
+        System.out.print("\t\t\t\t * STAGE *");             //  print out a header with the word "STAGE" surrounded by stars.
+        System.out.println("\t  \"O\"- available");
+        System.out.println("\t\t\t\t *********\n");
 
         for(int i=0 ; i<Rows ; i++){
-            System.out.print("\t\t");
+            System.out.print("\t");
             for(int x=0 ; x<5; x++){
-                System.out.print(" X ");
+                System.out.print(" "+seat_area[i][x]+" ");
             }
-            System.out.print(" ");
+            System.out.print("\t\t");
 
             for(int x=5 ; x<10; x++){
-                System.out.print(" X ");
+                System.out.print(" "+seat_area[i][x]+" ");
             }
             System.out.print("\n");
         }
     }
+
+
+    //____________________________ Receipt Function ----------------------------------------------------
 
     private static void Receipt(int seat, int Date){
         System.out.println("\t\tReceipt");

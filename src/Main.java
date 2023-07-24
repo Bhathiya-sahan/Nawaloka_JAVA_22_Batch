30fe442ea0c4592f00950bf829bd7c06f4ba2bb2
Bhathiya Sahan <bhathiyasahanedu@gmail.com>
2023-07-24 18:24:28 +0530

    Create But Ticket function and do some update on preint_seat function and receipt function

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;  // Import Scanner class to code
 public class Main {
     public static void main(String[] args) {
 
-        float standard,pensione,frequent;
+        float standard, pensione, frequent;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("\n-------------------------------------------------");
@@ -23,7 +23,7 @@ public class Main {
         System.out.print("Please Enter Number of Rows : ");
         int number_of_rows = scan.nextInt();
         System.out.print("Please Enter The Date Of The Show : ");
-        int date = scan.nextInt();
+        String date = scan.next();
 
         scan.nextLine();
         System.out.println("\nEnter The Seat Price Category");
@@ -35,17 +35,49 @@ public class Main {
         frequent = scan.nextFloat();
         System.out.println("\n-------------------------------------------------");
 
-        String [][] seat_list = new String [number_of_rows][10];
+        String[][] seat_list = new String[number_of_rows][10];
 
-        for(int i=0; i<number_of_rows; i++){
-            for (int j=0; j<10; j++){
-                seat_list [i][j] = "O";
+        for (int i = 0; i < number_of_rows; i++) {
+            for (int j = 0; j < 10; j++) {
+                seat_list[i][j] = "O";
             }
         }
-        print_seat(seat_list,number_of_rows);
+        buy_ticket(seat_list,date,number_of_rows);
+    }
+//____________________________ Buy Seat Function ----------------------------------------------------
+        private static void buy_ticket(String[][] seat_area, String date, int Rows){
+            Scanner scan = new Scanner(System.in);
+
+            System.out.print("How many seat do you need : ");
+            int seat = scan.nextInt();
+
+            for(int i=0; i<seat; i++) {
+                boolean gate1 = true;
+                System.out.println("\nEnter your "+(i+1)+" seat details.");
+                while(gate1) {
+
+                    System.out.print("Enter Row Number : ");
+                    int row_number = scan.nextInt();
+                    System.out.print("Enter Seat Number : ");
+                    int seat_number = scan.nextInt();
+
+                    if (seat_area[row_number-1][seat_number-1] == "O") {
+                        seat_area[row_number-1][seat_number-1] = "X";
+                        gate1=false;
+                    }
+                    else {
+                        System.out.println("This Seat also booked, try another one.");
+                    }
+                }
+            }
+            System.out.println("\nYour seats are booked.\nThanks for join with us");
+            System.out.println("\n-------------------------------------------------");
+            print_seat(seat_area, Rows);
+            receipt(seat,date);
+        }
 
 //____________________________ Print Seat Function ----------------------------------------------------
-    }
+
     private static void print_seat(String[][] seat_area, int Rows){
         System.out.print("\n\t\t\t\t *********");
         System.out.println("\t  \"X\"- Unavailable");
@@ -65,21 +97,22 @@ public class Main {
             }
             System.out.print("\n");
         }
+        System.out.println("\n-------------------------------------------------");
     }
 
 
     //____________________________ Receipt Function ----------------------------------------------------
 
-    private static void Receipt(int seat, int Date){
-        System.out.println("\t\tReceipt");
-        System.out.println("\t\t*******");
-        System.out.println("\t\tDate:"+Date);
+    private static void receipt(int seat, String Date){
+        System.out.println("\n\t\tReceipt");
+        System.out.println("\t\t*******\n");
+        System.out.println("\t\tDate : "+Date);
         System.out.println("\t\tNumber of seats booked : "+seat);
         System.out.println("\t\t"+seat+"* Standard @ $xx.xx =$ ");
         System.out.println("\t\t"+seat+"* Pensioner @ $xx.xx =$ ");
         System.out.println("\t\t"+seat+"* Frequent @ $xx.xx =$ ");
         System.out.println("\t\t\t\tTotal :$ ");
-
+        System.out.println("\n-------------------------------------------------");
     }
 
 }
